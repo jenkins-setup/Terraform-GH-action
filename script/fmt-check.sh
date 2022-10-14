d87c3aca69ccf5cf799b4d158368e2fbbd3b4c2f
uday-singh231 <uday.singh0231@gmail.com>
2022-10-14 17:45:39 +0530

    echo

--- script/fmt-check.sh
+++ script/fmt-check.sh
@@ -4,10 +4,8 @@ RET=0
 
 echo "$2"
 
-#for i in $(git rev-list "$1".."$2")
-
-    printf "check Terraform fmt "
-    echo "$1"
-    echo "another repo name is: "
-    echo "$2"
+printf "check Terraform fmt "
+echo "$1"
+echo "another repo name is: "
+echo "$2"
     

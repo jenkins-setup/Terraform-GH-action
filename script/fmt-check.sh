e7af74c5c1e388dbb52737d5dc1172957c088480
uday-singh231 <60268207+uday-singh231@users.noreply.github.com>
2022-10-14 19:09:57 +0530

    Pr dev (#16)
    * adding Guthub check
    
    * added script for check
    
    * added manual trigger for action
    
    * resolve typo
    
    * updated script step
    
    * echo test
    
    * echo
    
    * added terraform fmt check

--- script/fmt-check.sh
+++ script/fmt-check.sh
@@ -2,10 +2,10 @@
 
 RET=0
 
-for i in $(git rev-list "$1".."$2" )
-do
-    printf "check Terraform fmt "
-    echo "$1"
-    echo "another repo name is: "
-    echo "$2"
-done
+echo "$2"
+
+printf "check Terraform fmt "
+echo "$1"
+echo "another repo name is: "
+echo "$2"
+    

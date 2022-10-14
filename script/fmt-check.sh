e575a4fd09a1d5c3241bebde1b4943e9f2d5d2fc
uday-singh231 <uday.singh0231@gmail.com>
2022-10-14 17:25:25 +0530

    added script for check

--- script/fmt-check.sh
+++ script/fmt-check.sh
@@ -6,8 +6,7 @@ for i in $(git rev-list "$1".."$2" )
 do
     printf "check Terraform fmt "
     echo "$1"
-    echo "another repo is: "
+    echo "another repo name is: "
     echo "$2"
 done
-
 exit $RET

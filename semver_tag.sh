1dcd5fc8be8ee8263ce78afb022d65ea1eba4358
uday-singh231 <uday.singh0231@gmail.com>
2023-02-10 21:02:37 +0530

    updated script

--- semver_tag.sh
+++ semver_tag.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-desc=$(curl https://api.github.com/repos/jenkins-setup/Terraform-GH-action/pulls/$PR_NUMBER | jq '.body')
-echo $desc
+desc=$(curl https://api.github.com/repos/jenkins-setup/Terraform-GH-action/pulls/39 | jq '.body')
+
+if [[ $desc =~ .*"[x] Minor".* ]]
+    then
+        type="minor"
+    elif [[ $desc =~ .*"[x] Major".* ]]
+    then
+        type="major"
+    else
+        type="patch"
+    fi
+
+var=$(git fetch -t > /dev/null 2>&1 && git tag --sort=-v:refname | head -1)
+
+IFS=. read -r major minor patch <<EOF
+$var
+EOF
+
+case "$type" in
+major) 
+    tag="$((major+1)).0.0";
+    ;;
+
+minor)
+    tag="$major.$((minor+1)).0";
+    ;;
+
+*)
+    tag="$major.$minor.$((patch+1))";
+    ;;
+esac
+
+echo "$tag"
